c45535b7014b1036cecd3fab2fc600acceb88354
Daniel <danielpumayauli@gmail.com>
2021-09-04 03:51:36 -0500

    added show user endpoint

--- controllers/authController.go
+++ controllers/authController.go
@@ -11,6 +11,10 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type Claims struct {
+	jwt.StandardClaims
+}
+
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
 
@@ -63,7 +67,7 @@ func Login(c *fiber.Ctx) error {
 	}
 
 	claims := jwt.StandardClaims{
-		Id:        strconv.Itoa(int(user.Id)),
+		Issuer:    strconv.Itoa(int(user.Id)),
 		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // expires in 1 day
 	}
 
@@ -85,4 +89,27 @@ func Login(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"jwt": token,
 	})
+}
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil || !token.Valid {
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	claims := token.Claims.(*Claims)
+
+	var user models.User
+
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	return c.JSON(user)
+
 }
